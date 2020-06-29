c4d5fd3a9059c63315f106a095ae058df7e2692e
Jan Verhülsdonk <64151958+jan-v97@users.noreply.github.com>
2020-06-29 12:24:56 +0200

    new pics

--- test/ellipse/print.py
+++
@@ -1,8 +0,0 @@
-from paraview.simple import *
-canex2=OpenDataFile('C:\\Users\\janve\\local\\masterarbeit\\fenics\\test\\ellipse\\solution.pvd')
-clip=Clip()
-Hide(canex2)
-Show(clip)
-ResetCamera()
-Render()
-SaveScreenshot('C:\\Users\\janve\\local\\masterarbeit\\fenics\\test\\ellipse\\picture.jpg')
