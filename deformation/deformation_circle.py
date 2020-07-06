86d40bdfccddd1af6ac573234c3e78e3487f70d5
Jan Verhülsdonk <64151958+jan-v97@users.noreply.github.com>
2020-07-06 16:11:28 +0200

    deformation works

--- deformation/deformation_circle.py
+++ deformation/deformation_circle.py
@@ -73,6 +73,39 @@ class subdomaininput:
 
 #                                     functions for defining edges                                           
 
+# check if a point x is on the edge
+def on_polygon(x,edge):
+	for i in range(0,edge.pointnumber-1):
+		x0 = ((edge.data)[i]).x()
+		y0 = ((edge.data)[i]).y()
+		x1 = ((edge.data)[i+1]).x()
+		y1 = ((edge.data)[i+1]).y()
+		if (near(x[0],x0,tol) and near(x[1],y0,tol) or near(x[0],x1,tol) and near(x[1],y1,tol)):
+			return True
+		if near(x1,x0,tol):
+			r = (x1 - x0) * (x[1] - y0) / (y1 - y0) + x0 - x[0] 
+			if (near(r, 0.0, tol) and (((x[1] >= y0) and (x[1] <= y1)) or ((x[1] >= y1) and (x[1] <= y0)))):
+				return True
+		else:
+			r = (y1 - y0) * (x[0] - x0) / (x1 - x0) + y0 - x[1]
+			if (near(r, 0.0, tol) and (((x[0] >= x0) and (x[0] <= x1)) or ((x[0] >= x1) and (x[0] <= x0)))):
+				return True
+		#print ("( ", x0, " , ", y0, " ) ; ( ", x[0], " , ", x[1], " ) ; ( ", x1, " , ", y1, " )           return_val: ", result)
+	return False
+
+# define the identity
+class Identity(UserExpression):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+	def eval_cell(self,values,x,cell):
+		values[0] = x[0]
+		values[1] = x[1]
+
+	def value_shape(self):
+		return (2,)
+
+
 # functions to approximate the circle by a polygon
 def polygon_top(circle_points,r):
 	polygon = []
@@ -90,40 +123,65 @@ def polygon_bottom(circle_points,r):
 	polygon.append(dolfin.Point(r,0.))
 	return polygon
 
-	def on_polygon(self,x,on_boundary):
-		for i in range(0,edge.pointnumber):
-			x1 = ((self.data)[i]).x()
-			y1 = ((self.data)[i]).y()
-			x2 = ((self.data)[i+1]).x()
-			y2 = ((self.data)[i+1]).y()
-#check      x-1 parallel to 2-1                             
-			if near((x2-x1)/(x[0]-x1),(y2-y1)/(y[0]-y1),tol) & (x2-x1)/(x[0]-x1) < 1+tol & (x2-x1)/(x[0]-x1) > -tol:
-				return true
-		return false
+# transformations on the boundaries
+def_ell_x = 'sqrt(a0*a0*pow(cos(atan(a0*a0*tan(atan2(x[1],x[0])-a1))),2)+pow(sin(atan(a0*a0*tan(atan2(x[1],x[0])-a1))),2)/(a0*a0))*x[0]'
+def_ell_y = 'sqrt(a0*a0*pow(cos(atan(a0*a0*tan(atan2(x[1],x[0])-a1))),2)+pow(sin(atan(a0*a0*tan(atan2(x[1],x[0])-a1))),2)/(a0*a0))*x[1]'
+def_ell_quer = '(((L-abs(x[0]))/(L-r))*sqrt(a0*a0*pow(cos(atan(a0*a0*tan(atan2(x[1],x[0])-a1))),2)+pow(sin(atan(a0*a0*tan(atan2(x[1],x[0])-a1))),2)/(a0*a0))+((abs(x[0])-r)/(L-r)))*x[0]'
 
-#                                               input parameters, edges, domains                                          
+
+#                                               input parameters, edges, bc, domains                                          
 
 # input parameters for computational domain
-circle_points = 25
+circle_points = 15
 r = 0.5
 L = 1.1
-resolution = 50
-alpha=[1.5,1.]
+resolution = 80
+alpha=[1.8,0.5*pi]
+
 
 # input edges
 left_bottom = edgeinput([dolfin.Point(-L,0.),dolfin.Point(-L, -L)],Expression(('x[0]','x[1]'),degree=1))
 left_top = edgeinput([dolfin.Point(-L,L),dolfin.Point(-L, 0.)],Expression(('x[0]','x[1]'),degree=1))
 right_bottom = edgeinput([dolfin.Point(L, -L),dolfin.Point(L,0.)],Expression(('x[0]','x[1]'),degree=1))
 right_top = edgeinput([dolfin.Point(L,0),dolfin.Point(L, L)],Expression(('x[0]','x[1]'),degree=1))
-left = edgeinput([dolfin.Point(-L,0.),dolfin.Point(-r, 0.)],Expression(('x[0]','x[1]'),degree=1))
-right = edgeinput([dolfin.Point(L,0.),dolfin.Point(r, 0.)],Expression(('x[0]','x[1]'),degree=1))
+left = edgeinput([dolfin.Point(-L,0.),dolfin.Point(-r, 0.)],Expression((def_ell_quer,'x[1]'),degree=1,a0=alpha[0],a1=alpha[1],r=r,L=L))
+right = edgeinput([dolfin.Point(L,0.),dolfin.Point(r, 0.)],Expression((def_ell_quer,'x[1]'),degree=1,a0=alpha[0],a1=alpha[1],r=r,L=L))
 bottom = edgeinput([dolfin.Point(-L,-L ),dolfin.Point(L,-L)],Expression(('x[0]','x[1]'),degree=1))
 top = edgeinput([dolfin.Point(L,L ),dolfin.Point(-L,L)],Expression(('x[0]','x[1]'),degree=1))
-circle_top = edgeinput(polygon_top(circle_points,r),Expression(('x[0]','x[1]'),degree=1))
-circle_bottom = edgeinput(polygon_bottom(circle_points,r),Expression(('x[0]','x[1]'),degree=1))
+circle_top = edgeinput(polygon_top(circle_points,r),Expression((def_ell_x,def_ell_y),degree=1,a0=alpha[0],a1=alpha[1]))
+circle_bottom = edgeinput(polygon_bottom(circle_points,r),Expression((def_ell_x,def_ell_y),degree=1,a0=alpha[0],a1=alpha[1]))
+
 # define a vector with the edges
 edges = [left_bottom,left_top,right_bottom,right_top,left,right,bottom,top,circle_top,circle_bottom]
 edges_number = len(edges)
+
+
+# define the boundary conditions
+def boundary_left_bottom(x, on_boundary):
+	return on_polygon(x,left_bottom)
+def boundary_left_top(x, on_boundary):
+	return on_polygon(x,left_top)
+def boundary_right_bottom(x, on_boundary):
+	return on_polygon(x,right_bottom)
+def boundary_right_top(x, on_boundary):
+	return on_polygon(x,right_top)
+def boundary_left(x, on_boundary):
+	return on_polygon(x,left)
+def boundary_right(x, on_boundary):
+	return on_polygon(x,right)
+def boundary_bottom(x, on_boundary):
+	return on_polygon(x,bottom)
+def boundary_top(x, on_boundary):
+	return on_polygon(x,top)
+def boundary_circle_top(x, on_boundary):
+	return on_polygon(x,circle_top)
+def boundary_circle_bottom(x, on_boundary):
+	return on_polygon(x,circle_bottom)
+
+# define a vector with the boundary conditions
+boundary_edges = [boundary_left_bottom,boundary_left_top,boundary_right_bottom,boundary_right_top,boundary_left,boundary_right,boundary_bottom,boundary_top,boundary_circle_top,boundary_circle_bottom]
+
+
 
 # define the complete domain
 domain_complete = subdomaininput([left_top,left_bottom,bottom,right_bottom,right_top,top],[0,0,0,0,0,0],0)
@@ -132,6 +190,7 @@ domain_complete = subdomaininput([left_top,left_bottom,bottom,right_bottom,right
 domain_bottom = subdomaininput([left_bottom,bottom,right_bottom,right,circle_bottom,left],[0,0,0,0,1,1],0)
 domain_top = subdomaininput([right_top,top,left_top,left,circle_top,right],[0,0,0,0,1,1],0)
 domain_circle = subdomaininput([circle_top,circle_bottom],[0,0],0)
+
 # define a vector with the subdomains
 subdomains = [domain_bottom,domain_top,domain_circle]
 subdomain_number = len(subdomains)
@@ -145,9 +204,10 @@ domain = Polygon(domain_complete.get_polygon())
 for i in range(0, subdomain_number):
 	domain.set_subdomain (i+1, Polygon((subdomains[i]).get_polygon()))
 
+# generat the mesh
 mesh = generate_mesh (domain, resolution)
 
-
+# defining coefficients on subdomains
 X = FunctionSpace (mesh, "DG", 0)
 dm = X.dofmap()
 sudom = MeshFunction ('size_t', mesh, 2, mesh.domains())
@@ -161,18 +221,30 @@ def sudom_fct (sudom_arr, vals, fctspace):
 
 chi_a = sudom_fct (sudom_arr, [0,0,0,1], X)
 
-
+# create the function space
 W = VectorFunctionSpace(mesh, 'P', 1)
 
 # define dirichlet BC
-#bcs = []
-#for i in range(0,edges_number):
-#	bc = DirichletBC(W,(edges[i]).deformation, edges[i].on_polygon)
-#	bcs.append(bc)
+bcs = []
+for i in range(0,edges_number):
+	bc = DirichletBC(W,(edges[i]).deformation, boundary_edges[i])
+	bcs.append(bc)
 
 
-#Compute solution
-V = VectorFunctionSpace(mesh, 'P', 1)
-u = Function(V)
+# compute the deformation u
+u=Function(W)
+v=TestFunction(W)
+a = inner(grad(u),grad(v)) *dx
+L = 0
+solve(a == L, u, bcs)
+
+# compute the displacement
+id = project(Identity(),W)
+displacement = project(u-id,W)
+
+
+# safe displacement
 vtkfile = File('grid/const.pvd')
 vtkfile << chi_a
+vtkfile = File('grid/displacement.pvd')
+vtkfile << displacement
---
+++ deformation/jitfailure-dolfin_expression_0a12a7b9842bfb2a913577e7c1becce5/dolfin_expression_0a12a7b9842bfb2a913577e7c1becce5.cpp
@@ -0,0 +1,100 @@
+
+// Based on https://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+    #ifdef __GNUC__
+        #define DLL_EXPORT __attribute__ ((dllexport))
+    #else
+        #define DLL_EXPORT __declspec(dllexport)
+    #endif
+#else
+    #define DLL_EXPORT __attribute__ ((visibility ("default")))
+#endif
+
+#include <dolfin/function/Expression.h>
+#include <dolfin/math/basic.h>
+#include <Eigen/Dense>
+
+
+// cmath functions
+using std::cos;
+using std::sin;
+using std::tan;
+using std::acos;
+using std::asin;
+using std::atan;
+using std::atan2;
+using std::cosh;
+using std::sinh;
+using std::tanh;
+using std::exp;
+using std::frexp;
+using std::ldexp;
+using std::log;
+using std::log10;
+using std::modf;
+using std::pow;
+using std::sqrt;
+using std::ceil;
+using std::fabs;
+using std::floor;
+using std::fmod;
+using std::max;
+using std::min;
+
+const double pi = DOLFIN_PI;
+
+
+namespace dolfin
+{
+  class dolfin_expression_0a12a7b9842bfb2a913577e7c1becce5 : public Expression
+  {
+     public:
+       double alpha0;
+double alpha1;
+
+
+       dolfin_expression_0a12a7b9842bfb2a913577e7c1becce5()
+       {
+            _value_shape.push_back(2);
+       }
+
+       void eval(Eigen::Ref<Eigen::VectorXd> values, Eigen::Ref<const Eigen::VectorXd> x) const override
+       {
+          values[0] = (((L-abs(x[0]))/(L-r))*sqrt(a0*a0*pow(cos(atan2(a0*a0*tan(atan2(x[1],x[0])-a2))),2)+pow(sin(atan2(a0*a0*tan(atan2(x[1],x[0])-a2))),2)/(a0*a0))+((abs(x[0])-r)/(L-r)))*x[0];
+          values[1] = x[1];
+
+       }
+
+       void set_property(std::string name, double _value) override
+       {
+          if (name == "alpha0") { alpha0 = _value; return; }          if (name == "alpha1") { alpha1 = _value; return; }
+       throw std::runtime_error("No such property");
+       }
+
+       double get_property(std::string name) const override
+       {
+          if (name == "alpha0") return alpha0;          if (name == "alpha1") return alpha1;
+       throw std::runtime_error("No such property");
+       return 0.0;
+       }
+
+       void set_generic_function(std::string name, std::shared_ptr<dolfin::GenericFunction> _value) override
+       {
+
+       throw std::runtime_error("No such property");
+       }
+
+       std::shared_ptr<dolfin::GenericFunction> get_generic_function(std::string name) const override
+       {
+
+       throw std::runtime_error("No such property");
+       }
+
+  };
+}
+
+extern "C" DLL_EXPORT dolfin::Expression * create_dolfin_expression_0a12a7b9842bfb2a913577e7c1becce5()
+{
+  return new dolfin::dolfin_expression_0a12a7b9842bfb2a913577e7c1becce5;
+}
+
